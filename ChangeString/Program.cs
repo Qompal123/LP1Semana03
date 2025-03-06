413912ed02f2f85a0023b2fa5252b32c7f3c17b2
Zuha Rosario <zuhalourenco@gmail.com>
2025-03-06 22:53:51 +0000

    Add ChangeString

---
+++ ChangeString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Write something");
+
+            string frase = Console.ReadLine();
+
+            Console.WriteLine("Write a character");
+
+            string x = Console.ReadLine();
+
+            char character = char.Parse(x);
+
+            for (int i = 0; i < frase.Length; i++)
+            {
+                if (frase[i] == character)
+                {
+                    Console.Write("x");
+                }
+                else
+                {
+                    Console.Write(frase[i]);
+                }
+
+            }
+
+
+        }
+    }
+}
