1ed3f8ef0781e8b39ec7fd4f613bff9c123d9e8d
Zuha Rosario <zuhalourenco@gmail.com>
2025-03-06 22:23:50 +0000

    Add RightSize

---
+++ RightSize/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (string i in args)
+            {
+
+                if (i.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+
+                if (i.Length > 3)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
